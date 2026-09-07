861ea38fc91ae629aaf721473ef0cb62d7cc1ae7
calebreategui800-droid <calebreategui800@gmail.com>
2026-09-07 22:34:37 +0000

    Configuración inicial del proyecto Spring Boot con Spring Web

---
+++ inventariopedidos/src/main/java/pe/edu/utp/inventariopedidos/InventariopedidosApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.utp.inventariopedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventariopedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventariopedidosApplication.class, args);
+	}
+
+}
